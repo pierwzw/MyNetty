56a2472cef6a567d776eba7cce408c1203c8516e
zhongweiwu <zhongweiwu@pptv.com>
2018-06-25 16:27:00 +0800

    new netty demo

---
+++ src/main/java/client/EchoClientHandler.java
@@ -0,0 +1,35 @@
+package client;
+
+/**
+ * @auther zhongweiwu
+ * @date 2018/6/25 16:22
+ */
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+@Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//当被通知Channel是活跃的时候，发送一条消息
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Nettyrocks!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println(
+                "Clientreceived:" + byteBuf.toString(CharsetUtil.UTF_8)
+        );
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
